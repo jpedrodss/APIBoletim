72ef128aa55fcd1fcc979a45d0cb696047df9fe7
João Pedro <jpedrodss@gmail.com>
2020-09-01 09:13:47 -0300

    Initial commit

---
+++ Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces
+{
+    interface IAluno
+    {
+        List<Aluno> ListarTodos();
+        Aluno Cadastrar(Aluno a);
+        Aluno BuscarID(int id);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+    }
+}
